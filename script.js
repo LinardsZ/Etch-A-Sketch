21d5c836699207f11783020dcf7a93c82979585a
Linards Zilitis <l.zilitis@gmail.com>
2022-10-02 00:06:32 +0300

    add listeners for interacting with sketch grid

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+window.addEventListener('load', createGridItems);
+
+function createGridItems() {
+  const grid = document.querySelector(".grid-container");
+  grid.style = "grid-template-rows: repeat(16, 1fr); grid-template-columns: repeat(16, 1fr);"
+  for (let i = 0; i < 16*16; ++i) {
+    let newGridItem = document.createElement("div");
+    newGridItem.classList.add("grid-item");
+    grid.appendChild(newGridItem);
+  }
+
+  attachEventListeners();
+}
+
+function attachEventListeners() {
+  let gridItems = document.querySelectorAll(".grid-item");
+
+  gridItems.forEach(item => {
+    item.addEventListener("click", function() {
+      this.style = "background: black;";
+    });
+  });
+}
